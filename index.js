3c5de4858e13259a8617bb1bfc2c8cc23775d464
Kaguya2021 <mistermojenez@gmail.com>
2026-08-04 00:56:41 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Раздаем статические файлы (index.html, стили, архив bootloader_unlocker.zip)
+app.use(express.static(path.join(__dirname)));
+
+app.listen(PORT, () => {
+    console.log(`Сервер успешно запущен на порту ${PORT}`);
+});
+
